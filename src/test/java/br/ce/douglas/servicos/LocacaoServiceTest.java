2b3a634f45f92aa828f4101ecaaf75bd6fa819f2
dldouglaslopes <dldouglaslopes@gmail.com>
2020-03-02 11:20:05 -0300

    Mockito

--- src/test/java/br/ce/douglas/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/douglas/servicos/LocacaoServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
 
 import br.ce.douglas.daos.LocacaoDAO;
 import br.ce.douglas.daos.LocacaoDAO2;
@@ -38,7 +39,7 @@ public class LocacaoServiceTest {
 	@Before
 	public void setup() {
 		locacaoService = new LocacaoService();
-		LocacaoDAO dao = new LocacaoDAO2();
+		LocacaoDAO dao = Mockito.mock(LocacaoDAO.class);
 		locacaoService.setLocacaoDAO(dao);
 	}
 	
